4b8b0d7800e45db59f5154dc506ef14b3388ea01
jlach19 <joselesmes76@gmail.com>
2024-07-02 23:18:01 -0400

    Actualizado

--- Semana 9/exercise1.cpp
+++ Semana 9/exercise1.cpp
@@ -1,18 +1,56 @@
+/**
+ * Primeramente, para representar la cola usamos listas enlazadas. 
+ * Es por eso que creamos una clase llamada "Elem" que representa a cada elemento de la cola,
+ * dichos elementos estaran enlazados ya que tienen un apuntador dentro de la clase que apunta 
+ * al siguiente elemento de la cola ("next"). Ademas, tambien tiene un valor entero que representa
+ * el valor del elemento.
+ * 
+ * Luego, creamos una clase que representa la cola llamada "Queue" y que contendra dos apuntadores: uno que apunta
+ * al primer elemento de la cola ("front") y el otro apunta al ultimo elemento de la cola ("rear"). Ademas, usamos el metodo
+ * constructor para que en cada instancia de la la clase se inicialicen estos apuntadores como nulos.
+ * 
+ * Luego, creamos el metodo para insertar un elemento a la cola, dicho metodo se llama "enqueue" y recibe como parametro un valor entero. Dentro de este metodo 
+ * se crea un nuevo elemento de la cola con el valor que fue pasado como argumento al metodo y el puntero "next" sera nulo. Luego tanto el apuntador "front" como el apuntador 
+ * "rear" apuntaran a este nuevo elemento, en caso de que se este insertando el primer elemento a la cola; o, en caso de que no sea el primer elemento que se esta insertando
+ * en la cola, entonces el puntero "next" del puntero "rear" apuntara al nuevo elemento y luego asignamos al puntero "rear" que apunte a este nuevo elemento (ya que es el
+ * es el elemento insertado y sera el ultimo en la cola).
+ * 
+ * Posteriormente, dentro de la misma clase hay otro metodo que es para eliminar el primer elemento de la cola, este metodo es llamado "dequeue".
+ * Donde primero verifica que la cola esta vacia o no, si esta vacia no hace nada el metodo y si no lo esta entonces crea un apuntador que lo
+ * utilizaremos como auxiliar y que representara al primer elemento de la cola, luego el "front" apuntara al elemento inmediato que le sigue
+ * y en caso de que el elemento a eliminar sea el unico que hay en la cola entonces entonces, ademas de que "front" seria un puntero nulo,
+ * "rear" pasara a ser un puntero nulo y por ultimo en este metodo se procede a eliminar el apuntador auxiliar (el primer elemento de la cola).
+ * 
+ * Ademas, tendremos otros dos metodos en esta clase: Uno es un metodo llamado "front_queue" que nos retorna el valor del primer elemento de la cola
+ * y el otro metodo llamado "is_empty" que verifica si la cola esta vacia retornando un valor booleano.
+ * 
+ * Posteriormente, en la funcion main, se declara una variable "t" que representa la cantidad de consultas y estas son recibidas como input
+ * y se declara una variable del tipo "Queue" que representa la cola. Luego, creamos un ciclo while y dentro de el se declara una variable "x"
+ * que representa la opcion de la consulta que sera recibido como input. Seguidamente creamos un condicional else if donde se verifica si "x" es igual 
+ * a uno (1) entonces se declara una variable "n" que representa un entero el cual va ser el que añadiremos a la cola y sera recibido como
+ * input y luego llamamos el metodo "enqueue" de la clase "Queue" pasandole como argumento a "n" para añadir el elemento a la cola; en caso de que
+ *  "x" no sea uno (1) verifica si "x" es igual a dos (2) entonces  eliminamos el primer elemento de la cola por medio del metodo "dequeue" de la clase "Queue" y si "x" 
+ * no es ni uno (1) ni dos (2) entonces creamos a otro condicional if para imprimir el string "Empty!" en caso de que la cola sea vacio (para esto se usa
+ * el metodo "is_empty" de la clase "Queue" en la condicion del if) y en caso de que no sea vacio entonces imprime por consola el valor del primer elemento de la cola a 
+ * traves del metodo "front_queue" de la clase "Queue" y luego salimos de todos los condicionales para restarle a uno (1) al valor de "t" y volvera a comenzar el ciclo. 
+ * 
+ * El programa finaliza cuando se hayan realizado todas las consultas, es decir, cuando "t" valga cero.
+ */
 #include <iostream>
 using namespace std;
 
 // Clase que representa los elementos de la cola.
 struct Elem
 {
-    int date; // Valor del elemento.
-    Elem *next; // apuntador al siguiente elemento.
+    int data; // Valor del elemento.
+    Elem *next; // Apuntador al siguiente elemento.
 };
 
 // Clase que representa la cola.
 struct Queue
 {
-    Elem *front; // Apuntador al primer elemento de la cola
-    Elem *rear; // Apuntador al ultimo elemento de la cola
+    Elem *front; // Apuntador al primer elemento de la cola.
+    Elem *rear; // Apuntador al ultimo elemento de la cola.
 
     // Constructor
     Queue()
@@ -21,20 +59,20 @@ struct Queue
         rear = nullptr;
     }
 
-    // Metodo para insertar un elemento en la cola
+    // Metodo para insertar un elemento en la cola.
     void enqueue(int x) 
     {
-        // Creo un nuevo elemento para la cola
+        // Se crea un nuevo elemento para insertar a la cola.
         Elem* new_element = new Elem;
-        new_element -> date = x;
+        new_element -> data = x;
         new_element -> next = nullptr;
 
         if (front == nullptr) 
         {
             /**
              * Inserta el primer elemento a la cola.
-             * Por lo tanto, inicializamos los apuntadores
-             * de front y rear pra que ambos apunten a este
+             * Por lo tanto, hacemos que los apuntadores
+             * de "front" y "rear" apunten a este
              * primer elemento que se esta insertando.
              */
             front = new_element;
@@ -46,16 +84,16 @@ struct Queue
              * el primer elemento que se inserta.
              * Por lo tanto, el apuntador "next" del apuntador
              * "rear" apuntara al nuevo elemento que se esta
-             * insertando en la cola yla variable rear apuntara a
+             * insertando en la cola y el puntero "rear" apuntara a
              * ese nuevo elemento insertado. Lo que indica que este
-             * elemento es el ultimo de la cola.
+             * elemento es ahora el ultimo de la cola.
              */
             rear -> next = new_element;
             rear = new_element;
         }
     }
 
-    // Metodo que elimina un elemento de la cola
+    // Metodo que elimina el primer elemento de la cola
     void dequeue() 
     {
         // Verificacion de que la cola esta vacia.
@@ -65,10 +103,11 @@ struct Queue
         }
 
         /**
-         * Creamos un apuntador auxiliar que se inicializa
-         * apuntando a front. Luego hacemos que el elemento al que esta 
+         * Se crea un apuntador auxiliar que se inicializa
+         * apuntando a "front". Luego hacemos que el elemento al que esta 
          * apuntando "next" del apuntador de "front" sea el 
-         * nuevo elemento al que apuntara "front".
+         * al que ahora apuntara "front". Puede ser que dicho elemento
+         * que esta apuntado "next" sea nulo y en ese caso "front" seria un puntero nulo.
          */
         Elem *aux = front;
         front = front -> next;
@@ -92,10 +131,10 @@ struct Queue
         delete aux; // Eliminamos "aux" que es primer elemento de la cola.
     }
 
-    // Metodo que retorna el primer elemento de la cola.
+    // Metodo que retorna el valor del primer elemento de la cola.
     int front_queue() 
     {
-        return front -> date;
+        return front -> data;
     }
 
     // Metodo que verifica si la cola está vacia.
@@ -123,19 +162,19 @@ int main()
             my_queue.enqueue(n); // Se añade el elemento a la cola.
         } else if ( x == 2 )
         {
-            my_queue.dequeue(); // Eliminamos el primer elemento de la cola
+            my_queue.dequeue(); // Se elimina el primer elemento de la cola.
         } else
         {
             if (my_queue.is_empty())
             {
-                printf("Empty!\n"); // Imprime por consola el string "Empty!" si la cola es vacia
+                printf("Empty!\n"); // Imprime por consola el string "Empty!" si la cola es vacia.
             } else 
             {
-                printf("%d\n", my_queue.front_queue()); // Imprime el valor del primer elemento de la cola
+                printf("%d\n", my_queue.front_queue()); // Imprime el valor del primer elemento de la cola si no esta vacia.
             }
         }
         
-        t -= 1; // Se resta uno (1) al valor de "t"
+        t--; // Se resta uno (1) al valor de "t".
     }
 
     return 0;
--- Semana 9/exercise2.cpp
+++ Semana 9/exercise2.cpp
@@ -1,3 +1,47 @@
+/**
+ * Primeramente, para representar la pila usamos listas doblemente enlazadas.
+ * Por lo tanto, creamos una clase llama "Elem" que representa a los elementos de la pila, dichos
+ * elementos estaran enlazados ya que tienen dos apuntadores dentro de esta clase: uno apunta al 
+ * elemento anterior que esta hacia arriba en la pila que lo llamamos "previous" y otro que apunta
+ * al elemento siguiente que esta hacia abajo en la pila que lo llamamos "next". Ademas, esta clase 
+ * tambien tiene un valor entero que representa el valor del elemento.
+ * 
+ * Luego, se crea una clase llamada "Stack" que representa a la pila y contendra al apuntador "top" que 
+ * apunta al elemento que esta en la parte superior de la pila. Luego, creamos el metodo contructor de la clase
+ * para que en cada instancia inicialice al puntero "top" como nulo.
+ * 
+ * Luego, creamos el metodo "push" que recibe como parametro un entero. Este metodo lo que hace es insertar un elemento
+ * a la pila. El metodo crea el elemento para insertar en la pila con el valor del parametro que le pasamos como argumento
+ * al metodo, el apuntador "next" del nuevo elemento apuntara a "top" y el apuntador "previous" del nuevo elemento 
+ * sera nulo. Seguidamente, verificamos si "top" es distinto de nulo por medio de un condicional if para que su 
+ * puntero "previous" apunte al nuevo elemento y luego (fuera del if) hacemos que "top" apunte a ese nuevo elemento.
+ * 
+ * Posteriormente, se crea un metodo para eliminar el elemento de la pila que esta en la parte superior.
+ * Este metodo se llama "pop" y dentro de este metodo se verifica por medio de un condicional if si la pila esta 
+ * vacia (en caso de que lo este no se hace nada en este metodo) si la pila tiene algun elemento entonces se crea
+ * un puntero temporal que apuntara a "top" y luego "top" hacemos que apunte al puntero "next" del puntero "top".
+ * Luego, creamos un condicional pare verificar si "top" es nulo, de ser cierto entonces su puntero "previous" lo asignamos como nulo
+ * y salimos del if para que eliminemos ese puntero temporal que esta representando el elemento de la pila
+ * que esta en la parte superior de la pila.
+ * 
+ * Posteriormente, se crean dos metodos mas: uno es "top_stack" que es para retornar el valor del elemento que esta
+ * en la parte superior de la pila y el otro es "is_empty" que verifica si la pila esta vacia y retorna un valor booleano.
+ * 
+ * Luego, en la funcion main declaramos una variable "t" que representa la cantidad de consultas que seran recibidas
+ * como input, se crea una variable de tipo "Stack" que representa la pila y luego creamos a un ciclo while.
+ * Dentro de este ciclo se declara una variable "x" que representa la opcion de la consulta y sera recibido
+ * como input. Ademas, se crea un else if que primero verifica si "x" es igual a uno (1), de ser cierto entonces
+ * se declara una variable "n" que representa el numero que se va añadir a la pila y se recibe como input para
+ * luego llamar al metodo "push" de la clase "Stack" y pasarle como argumento la variable "n" para que añada el elemento a la pila; 
+ * en caso de que "x" no sea (uno) 1, entonces se verifica que "x" sea igual a (dos) 2 y en caso de ser cierto se
+ * elimina el elemento de la parte superior de la pila a traves del metodo "pop" de la clase "Stack"; en caso de 
+ * que "x" no sea ni uno (1) ni dos (2) entonces se crea un condicional if else que verifica si la pila esta 
+ * vacia por medio del metodo "is_empty" de la clase "Stack", si es cierto entonces imprime por consola el string
+ * "Empty!"; en caso contrario, imprime por consola el valor del elemento de la parte superior de la pila por medio 
+ * del metodo "top_stack" de la clase "Stack". Luego se le resta uno (1) al valor de "t" y comienza nuevamente el ciclo.
+ * 
+ * El programa finaliza cuando ya se hayan realizado todas las consultas, es decir, cuando "t" sea igual a cero.
+ */
 #include <iostream>
 using namespace std;
 
@@ -117,7 +161,7 @@ int main()
             }
         }
         
-        t--;
+        t--; // Se le resta uno (1) al valor de "t".
     }
     
     return 0;
